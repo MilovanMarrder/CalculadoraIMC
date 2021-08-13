b18c13b03319745be7f4c60463f056373413ca63
MilovanMarrder <milovanmarrder@outlook.com>
2021-08-12 22:57:54 -0600

    Avance 1 en estructura
    Se define el funcionamiento del calculo con JS. Sin embargo, queda pendiente resolver la impresion en pantalla del resultado con el mensaje "NaN"

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-//Prueba del escript
-console.log('Hola')
+function calcularIMC (){
+    var altura = parseFloat(document.querySelector('#altura').Value);
+    var peso = parseFloat(document.querySelector('#peso').Value);
+    var resultado = parseFloat((peso/2.2)/((altura/100)*(altura/100)));
+
+    return document.querySelector('#aquiRespuesta').innerHTML = `Tu IMC es `+ resultado;
+}
