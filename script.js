1777c484a2ef235e14c51f5f18fcaa92a079b5a7
MilovanMarrder <milovanmarrder@outlook.com>
2021-08-13 21:43:19 -0600

    V.1.0
    Estructura completamente funcional y se le agregó algo de estiilos (aún no terminado)

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function calcularIMC (){
-    var altura = parseFloat(document.querySelector('#altura').Value);
-    var peso = parseFloat(document.querySelector('#peso').Value);
-    var resultado = parseFloat((peso/2.2)/((altura/100)*(altura/100)));
 
-    return document.querySelector('#aquiRespuesta').innerHTML = `Tu IMC es `+ resultado;
+function calcularIMC(){
+    var altura = parseFloat(document.getElementById("altura").value);
+    var peso = parseFloat(document.getElementById("peso").value);
+    var resultado = parseFloat((peso/2.2)/(altura * altura)).toFixed(2);
+    if(resultado > 30){
+        document.getElementById("RP").innerHTML= 'Tu Indice de masa corporal es: ' + resultado + " tienes obesidad";
+
+    }else if(resultado > 25){
+        document.getElementById("RP").innerHTML= 'Tu Indice de masa corporal es: ' + resultado + " tienes sobrepeso";
+
+    }else if(resultado > 20){
+        document.getElementById("RP").innerHTML= 'Tu Indice de masa corporal es: ' + resultado + " tienes un peso saludable";    
+    }else if(resultado <= 20){
+        document.getElementById("RP").innerHTML= 'Tu Indice de masa corporal es: ' + resultado + " estas delgado";
+    }
 }
